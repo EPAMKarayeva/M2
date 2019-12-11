6c45d457161760e96f1bbcbdbf654f5d729c231c
Wixiam <k.aziza.i@mail.ru>
2019-12-11 15:22:25 +0600

    2

---
+++ 2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayFindIndex
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+
+            int[] array = new int[n];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-5,5);
+                Console.Write(array[i]+" ");
+            }
+
+            SearchIndex searchIndex = new SearchIndex();
+
+            var index = searchIndex.FindIndex(array);
+
+            Console.WriteLine("\nResult:"+index);
+
+            Console.ReadKey();
+        }
+
+    }
+
+    public class SearchIndex
+    {
+        public int FindIndex(int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int sum=GetSumBeforeElement(array, i);
+                int sum1=GetSumAfterElement(array, i);
+
+                if (CompareSums(sum, sum1))
+                {
+                    temp = i;
+                }
+            }
+
+            if(temp == 0)
+            {
+                return -1;
+            }
+
+            return temp;
+            
+        }
+
+
+        public bool CompareSums(int i, int j)
+        {
+            if (i == j)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+
+        public int GetSumBeforeElement(int[] array, int x)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < x; i++)
+            {
+               sum += array[i];
+            }
+
+            return sum;
+
+        }
+
+        public int GetSumAfterElement(int[] array, int x)
+        {
+            int sum = 0;
+
+            for (int m = x+1; m < array.Length; m++)
+            {
+                sum += array[m];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Xunit;
+using ArrayFindIndex;
+
+
+namespace XUnitTestProject1
+{
+    public class UnitTest2
+    {
+
+        SearchIndex index = new SearchIndex();
+
+        [Fact(DisplayName ="Find correct index")]
+        public void TestForFindingCorrectIndex()
+        {
+            int[] array = { 2, 4, 1, 3, 3 };
+
+            var result = index.FindIndex(array);
+
+            int expected = 2;
+
+            Assert.Equal(result, expected);
+        }
+
+        [Fact(DisplayName = "Compare of sums")]
+        public void TestForCompareSums()
+        {
+            int sum = 10; 
+                
+            int sum1 = 10;
+
+            var result = index.CompareSums(sum, sum1);
+
+            var expected = true;
+
+            Assert.Equal(expected, result);
+           
+        }
+
+        [Fact(DisplayName = "Check for count correct sum before current element")]
+        public void TestForCorrectSumBeforeElement()
+        {
+            int[] array = { -2, 4, 1, -1, 3 };
+
+            var result = index.GetSumBeforeElement(array, 2);
+
+            var expected = 2;
+
+            Assert.Equal(expected, result);
+
+
+        }
+
+
+        [Fact(DisplayName = "Check for counting correct sum after current element")]
+        public void TestForCorrectSumAfterElement()
+        {
+            int[] array = { -2, 4, 1, -1, 3 };
+
+            var result = index.GetSumAfterElement(array, 2);
+
+            var expected = 2;
+
+            Assert.Equal(expected, result);
+
+
+        }
+    }
+}
