956a82353489af792694e67f37111147e07189f1
Wixiam <k.aziza.i@mail.ru>
2019-12-18 11:40:35 +0600

    1

--- M2_5/NextBiggerNumber.cs
+++ M2_5/NextBiggerNumber.cs
@@ -9,5 +9,80 @@ namespace M2_5
     public class NextBiggerNumber
     {
 
+
+        public int[] ConvertingToInt(string str)
+        {
+            var chrArray = str.ToCharArray();
+
+            var array = new int[str.Length];
+
+            for (int i = 0; i < chrArray.Length; i++)
+            {
+                var temp = chrArray[i].ToString();
+                array[i] = Convert.ToInt32(temp);
+            }
+
+            return array;
+
+        }
+
+        //public char [] SwapSymbols(string str, int m)
+        //{  
+        //    char[] charArray = str.ToCharArray();
+        //    char temp; 
+
+        //    for (int i =m; i <charArray.Length; i++)
+        //    {
+        //        for (int j = i + 1; j < charArray.Length; i++)
+        //        {
+        //            temp = charArray[i];
+        //            charArray[i] = charArray[j];
+        //            charArray[j] = temp;
+        //        }
+        //    }
+
+        //    return charArray;
+        //}
+
+        //public void FindNextBigger(string str)
+        //{
+        //    var chrArray = new char[str.Length];
+
+        //    for (int i = 0; i < chrArray.Length; i++)
+        //    {
+    
+
+        //    }
+
+        //}
+
+
+        public int[] NumberSorted(string str)
+        {
+            var array = ConvertingToInt(str);
+
+            int temp;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+            }
+
+            return array;
+
+
+        }
+
+
+
     }
 }
--- M2_5/Program.cs
+++ M2_5/Program.cs
@@ -10,8 +10,21 @@ namespace M2_5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter number:");
 
+            var str = Console.ReadLine();
 
+            NextBiggerNumber nextBiggerNumber = new NextBiggerNumber();
+
+            var array = nextBiggerNumber.NumberSorted(str);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
